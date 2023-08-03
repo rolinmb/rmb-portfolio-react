f479532dd95dafed6186d4c92959f88e95edcf92
Rolin Blake <rolinmblake@gmail.com>
2023-08-03 10:06:51 -0500

    styling updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,23 +4,22 @@ import React from 'react';
 function Home() {
   return (
     <div id='home-page' className='page-wrap'>
-      <h1>Hello</h1>
-      <br />
+      <h1>Greetings!</h1>
       <div className="home-content-wrap">
+        <h2>Education</h2>
         <ul id='education-list'>
           <li>
-		    <h3>Bachelor of Science</h3>
-		    <h3>Iowa State University - Ames, IA</h3>
-		    <h4><i>Major: </i>Mathematics <i>Minor: </i>Computer Science</h4>
-		    <p>Fall 2016 - Spring 2020</p>
-		  </li>
+		        <h3>Bachelor of Science</h3>
+		        <h3>Iowa State University - Ames, IA</h3>
+		        <h4><i>Major: </i>Mathematics <i>Minor: </i>Computer Science</h4>
+		        <p>Fall 2016 - Spring 2020</p>
+		      </li>
         </ul>
       </div>
       <br />
       <p>Versatile IT Support Specialist with programming, logistics, and quality assurance experience; Audio Engineer and Instrumentalist.</p>
-      <br />
       <div className='home-content-wrap'>
-        <h4>Skills</h4>
+        <h2>Skills</h2>
         <ul id='skills-list'>
           <li><b><i>Operating Systems: </i></b>Windows, Linux, macOS, chromeOS</li>
 		      <li><b><i>Programming Languages: </i></b>C, C++, C#, Go, Java, JavaScript, MATLAB, Python, SQL</li>
